21f1007ce08291b5b2e4218465db250ae4cb24df
zeus <wpdn79@likelion.org>
2019-11-11 17:07:40 +0900

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: "root",
+    password: null,
+    database: "nodebird",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "nodebird",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false
+  }
+};
